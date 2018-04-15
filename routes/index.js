f8d934c6a7119ce98fd94081373112c2375cc91f
Eabasir <eabasir@gmail.com>
2018-04-15 17:33:41 +0330

    first commit

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {title: 'Express'});
+});
+
+router.post('/order/invoice', function (req, res, next) {
+
+  let multiplier = -1;
+  if (req.body.return)
+    multiplier = 1;
+
+  const data = {
+    mobile_no: req.body.mobile_no,
+    barcode: req.body.barcode
+  };
+
+  const values = {
+    total_price: req.body.total_price,
+    used_point: req.body.used_point,
+    used_balance: req.body.used_balance
+  };
+
+  for(let key in values){
+    if(values.hasOwnProperty(key)){
+      values[key] += (multiplier * Math.floor(Math.random() * values[key]))
+    }
+  }
+
+  res.json(Object.assign(values, data));
+});
+
+
+module.exports = router;
